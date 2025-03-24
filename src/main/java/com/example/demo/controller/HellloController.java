107aa5571c6367a5a2291896b13438e7fc4d3e82
villageworks <villageworks@gmail.com>
2025-03-24 17:09:59 +0900

    step2: display input form for self introduction

--- src/main/java/com/example/demo/controller/HellloController.java
+++ src/main/java/com/example/demo/controller/HellloController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HellloController {
+	
+	@GetMapping("/hello")
+	public String input() {
+		return "input";
+	}
+	
 	@GetMapping("/") // URL「http://localhost:8080」
 	public String index() {
 		return "index";
