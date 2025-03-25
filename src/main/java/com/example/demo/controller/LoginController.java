c45be4f774961cf3cd3bd80c6a9fa97316228cdf
villageworks <villageworks@gmail.com>
2025-03-25 21:45:17 +0900

    ver.0.70: the login program

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class LoginController {
+	
+	/**
+	 * 認証用の定数群
+	 */
+	private static final String USER_ID = "student";
+	private static final String USER_PASSWORD = "himitu";
+	
+	@PostMapping("/login")
+	public String login(@RequestParam String id,       // ユーザID
+						@RequestParam String password, // パスワード
+						Model model // 遷移先画面に引き継ぐデータがある場合
+					   ) {
+		
+		// 取得したIDとパスワードによる認証
+		String nextPage = "";
+		if (USER_ID.equals(id) && USER_PASSWORD.equals(password)) {
+			nextPage = "index";
+		} else {
+			String message = "";
+			if (!USER_ID.equals(id)) {
+				message = "ユーザIDが一致しませんでした";
+			} else {
+				message = "パスワードが一致しませんでした";
+			}
+			model.addAttribute("message", message);
+			nextPage = "login";
+		}
+		
+		// 画面遷移
+		return nextPage;
+	}
+	@GetMapping("/login")
+	public String inndex() {
+		// 初期画面表示
+		return "login";
+	}
+	
+}
